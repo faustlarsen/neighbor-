adb91a9aaf5776d16631b854b749e4502efef5e9
faustlarsen <faustlarsen@gmail.com>
2020-10-08 00:10:01 -0700

    add a second form field that takes a name as an input along with Wont you be my neghbor

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
   $("#picknumber").submit(function(event) {
+    let name = $("#inputName").val();
     let input = $("#inputNumber").val();
-    
     let array = [];
     for (let i = 0; i <= input; i++) {
       let outcome = i.toString();
-      
-      
       if (outcome.includes(3)) {
-        array.push("Won't you be my neighbor?");
+        array.push(name + ", " + "Won't you be my neighbor?");
       } else if (outcome.includes(2)) {
         array.push("Boop");
       } else if (outcome.includes(1)) {
@@ -16,12 +14,9 @@ $(document).ready(function() {
       } else {
         array.push(outcome);
       }
-
     }
+
   
-
-    
-
 
   $("#output").text(array);
   event.preventDefault();
